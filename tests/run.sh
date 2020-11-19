7a3173a2d70a05f0e11cc17336e2d803218ed501
Chingis Sandanov <chingis@wodby.com>
2020-11-20 02:28:23 +0700

    Fixes

--- tests/run.sh
+++ tests/run.sh
@@ -8,8 +8,8 @@ fi
 
 docker-compose build --force-rm --no-cache python
 docker-compose up -d
-docker-compose exec postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker-compose exec -T postgres make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
 
-docker-compose exec python tests.sh
+docker-compose exec -T python tests.sh
 
 docker-compose down
