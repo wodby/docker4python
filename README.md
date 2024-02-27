cdee467b99c2a198dcbeb5485635b628425ce1a8
Chingis Sandanov <chingis@wodby.com>
2024-02-27 17:08:08 +0300

    Rename docker-compose to compose

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Introduction
 
-Docker4Python is a set of docker images optimized for Python applications (suitable for Django). Use `docker-compose.yml` file from the [latest stable release](https://github.com/wodby/docker4python/releases) to spin up local environment on Linux, Mac OS X and Windows. 
+Docker4Python is a set of docker images optimized for Python applications (suitable for Django). Use `compose.yml` file from the [latest stable release](https://github.com/wodby/docker4python/releases) to spin up local environment on Linux, Mac OS X and Windows. 
 
 - Read the docs on [**how to use**](https://wodby.com/docs/stacks/python/local#usage)
 - Ask questions on [Discord](http://discord.wodby.com/)
--- docker.mk
+++ docker.mk
@@ -11,9 +11,9 @@ help : docker.mk
 ## up	:	Start up containers.
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
-	docker-compose pull
-	docker-compose build
-	docker-compose up -d --remove-orphans
+	docker compose pull
+	docker compose build
+	docker compose up -d --remove-orphans
 
 .PHONY: mutagen
 mutagen:
@@ -22,7 +22,7 @@ mutagen:
 ## build	:	Build python image.
 build:
 	@echo "Building python image for for $(PROJECT_NAME)..."
-	docker-compose build
+	docker compose build
 
 ## down	:	Stop containers.
 down: stop
@@ -30,12 +30,12 @@ down: stop
 ## start	:	Start containers without updating.
 start:
 	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
-	@docker-compose start
+	@docker compose start
 
 ## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
-	@docker-compose stop
+	@docker compose stop
 
 ## prune	:	Remove containers and their volumes.
 ##		You can optionally pass an argument with the service name to prune single container
@@ -43,7 +43,7 @@ stop:
 ##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
 ## ps	:	List running containers.
 ps:
@@ -59,7 +59,7 @@ shell:
 ##		logs python	: View `python` container logs.
 ##		logs nginx python	: View `nginx` and `python` containers logs.
 logs:
-	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
 # https://stackoverflow.com/a/6273809/1826109
 %:
